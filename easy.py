69e2da71efd8caf9c39dc1c7e0871fc075e4a849
DaveKgw1 <45153558+davekgw@users.noreply.github.com>
2019-06-05 21:05:21 +0800

    Update easy.py

--- easy.py
+++ easy.py
@@ -7,32 +7,61 @@ from esmcore import *
 
 def main():
 	banner()
-	print "   [01] Information Gathering"
-	print "   [02] Exit the Easymux\n"
-	easymux = raw_input("esmx > ")
+	print "   [01] Tool For Get_Information"
+        print "   [02] Tool Facebook"
+        print "   [03] Tool Web\n"
+	print "   [00] Exit the Easymux\n"
+	easymux = raw_input("Easymux > ")
 	
 	if easymux == "01" or easymux == "1":
 		print "    [01] OSIF"
                 print "    [02] GPS_Tracking"
-                print "    [03] SpamChat\n"
 		print "    [00] Back to main menu\n"
-		infogathering = raw_input("esmx > ")
+		infogathering = raw_input("Info > ")
 		
 		if infogathering == "01" or infogathering == "1":
 			osif()
 		elif infogathering == "02" or infogathering == "2":
 			gps()
-		elif infogathering == "03" or infogathering == "3":
-			spamchat()
 		elif infogathering == "00" or infogathering == "0":
 			restart_program()
 		else:
 			print "\nERROR: Wrong Input"
 			timeout(2)
 			restart_program()
-	
-	elif easymux == "02" or easymux == "2":
-		sys.exit()
+
+        elif easymux == "02" or easymux == "2":
+                print "    [01] SpamChat"
+                print "    [02] UnFriendFB\n"
+		print "    [00] Back to main menu\n"
+		toolfb = raw_input("ToolFB > ")
+
+                if toolfb == "01" or toolfb == "1":
+			spamchat()
+                elif toolfb == "02" or toolfb == "2":
+			unfriend()
+                elif toolfb == "00" or toolfb == "0":
+			restart_program()
+		else:
+			print "\nERROR: Wrong Input"
+			timeout(2)
+			restart_program()
+
+	elif easymux == "03" or easymux == "3":
+                print "    [01] Web_Deface\n"
+                print "    [00] Back to main menu\n"
+                web = raw_input("ToolWeb > ")
+                
+                if web == "01" or web == "1":
+			webdef()
+                elif web == "00" or web == "0":
+			restart_program()
+                else:
+			print "\nERROR: Wrong Input"
+			timeout(2)
+			restart_program()
+        elif easymux == "00" or easymux == "0":		
+                sys.exit()
 	
 	else:
 		print "\nERROR: Wrong Input"
